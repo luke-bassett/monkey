ab64d8adfde69eebc45b5d072fd3f2cb24b77287
Luke Bassett <luke@pex.com>
2022-08-11 18:17:45 -0700

    finished ch1; lexer complete and started repl;

--- token/token.go
+++ token/token.go
@@ -19,6 +19,8 @@ const (
     SLASH    = "/"
     LT       = "<"
     GT       = ">"
+    EQ       = "=="
+    NOT_EQ   = "!="
 
     // Delimiters
     COMMA = ","
@@ -31,6 +33,11 @@ const (
     // Keywords
     FUNCTION = "FUNCTION"
     LET = "LET"
+    TRUE = "TRUE"
+    FALSE = "FALSE"
+    IF = "IF"
+    ELSE = "ELSE" 
+    RETURN = "RETURN"
 
 )
 
@@ -42,6 +49,11 @@ type Token struct {
 var keywords = map[string]TokenType{
     "fn": FUNCTION,
     "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
