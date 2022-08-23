53edf704ee600881f490b1550552d93dd29dccdd
Luke Bassett <luke@pex.com>
2022-08-22 23:07:55 -0700

    mid p41

--- parser/parser.go
+++ parser/parser.go
@@ -51,3 +51,43 @@ func (p *Parser) parseStatement() ast.Statement {
 		return nil
 	}
 }
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+    stmt := &ast.LetStatement{Token: p.curToken}
+
+    if !p.expectPeek(token.IDENT) {
+        return nil
+    }
+
+    stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+    if !p.expectPeek(token.ASSIGN) {
+        return nil
+    }
+
+	// TODO: We're skipping the expressions until we 
+    // encounter a semicolot
+	for !p.curTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+
+    return stmt
+}
+
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+    return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true 
+	} else {
+		return false 
+	}
+}
